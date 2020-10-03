485c4bbcd9bd2c34fe4716d525815160edd51190
Donghwan Seo <tonky0110@gmail.com>
2020-10-03 20:25:22 +0900

    2.16 res.locals를 사용한 파라메터 전달. 서버에서 웹페이지로 routes 정보를 전달하여 url의 파편화를 방지한다.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 
+// middlewares
+import {
+    localsMiddleware
+} from "./middelwares";
+
 // Routers
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
@@ -12,24 +17,25 @@ import videoRouter from './routers/videoRouter';
 // Routes
 import routes from './routes';
 
+
 const app = express();
 
 const handleHome = (req, res) => res.send("Hi home");
 
 const handleProfile = (req, res) => res.send("You are on my profile.");
 
+// middlware
+app.use(helmet());
 app.set('views', 'src/views')
 app.set('view engine', 'pug');
-
-// middlware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(helmet());
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
 // routes
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -10,8 +10,8 @@ import routes from '../routes';
 const userRouter = express();
 
 // userRouter.get(routes.home, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
