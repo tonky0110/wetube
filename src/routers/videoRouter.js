f7bc806ae161b97e4b444dc1d6d6e7f110b52923
Donghwan Seo <tonky0110@gmail.com>
2020-10-03 16:35:38 +0900

    2.11 Controller - url로 접속한 클라이언트를 처리하기 위한 controller를 생성하고 연결

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+    deleteVideo,
+    editVideo,
+    home,
+    upload,
+    videoDetail,
+    videos
+} from '../controllers/videoController';
+import routes from '../routes';
+
+const videoRouter = express.Router();
+
+// videoRouter.get(routes.home, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
+export default videoRouter;
