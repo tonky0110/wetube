d8d43617e95c5fa3f3661b659b57d1106bd632cd
Donghwan Seo <tonky0110@gmail.com>
2020-10-03 13:58:08 +0900

    2.4 handle*** url접속하면 해당하는 url에 따라 function을 찾아서 return해줌.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hi')
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+// app.get('/', function (req, res) {
+//     res.send('Hello world.');
+// });
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  console.log("Hi from home.");
+  res.send("Ho");
+}
+
+function handleProfile(req, res) {
+  console.log("Hi from profile.");
+  res.send("You are on my profile.");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
