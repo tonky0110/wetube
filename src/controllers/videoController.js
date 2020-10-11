95db2a5d319d3969403e157c1076e3690d3efa17
Donghwan Seo <tonky0110@gmail.com>
2020-10-11 21:17:44 +0900

    3.12 searching using reget and bug fixed(fileUrl)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,14 +22,24 @@ export const home = async (req, res) => {
     }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {
         query: {
             term: searchingBy = ""
         } = {}
     } = req;
-    console.log("searchingBy: ", searchingBy);
-    return res.render("search", {
+    let videos = [];
+    try {
+        videos = await Video.find({
+            title: {
+                $regex: searchingBy,
+                $options: "i"
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+    res.render("search", {
         pageTitle: "Search",
         searchingBy,
         videos,
