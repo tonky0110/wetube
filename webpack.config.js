ece9deb9502956793a0391ac259ee0e1495ec9bd
Donghwan Seo <tonky0110@gmail.com>
2020-10-11 22:23:25 +0900

    4.2.2 styles with webpack scss >> css & import

--- webpack.config.js
+++ webpack.config.js
@@ -8,44 +8,47 @@ const ENTRY_FILE = path.resolve(__dirname, "src", "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
-  mode: MODE,
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: "css-loader" },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "autoprefixer",
-                    {
-                      browsers: "cover 99.5%",
+    entry: ENTRY_FILE,
+    mode: MODE,
+    module: {
+        rules: [{
+            test: /\.(scss)$/,
+            use: [{
+                    loader: MiniCssExtractPlugin.loader
+                },
+                {
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                [
+                                    "autoprefixer",
+                                    {
+                                        browsers: "cover 99.5%",
+                                    },
+                                ],
+                            ],
+                        },
                     },
-                  ],
-                ],
-              },
-            },
-          },
-          { loader: "sass-loader" },
-        ],
-      },
+                },
+                {
+                    loader: "sass-loader"
+                },
+            ],
+        }, ],
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js",
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+        }),
     ],
-  },
-  output: {
-    path: OUTPUT_DIR,
-    filename: "[name].js",
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-  ],
 };
 
 module.exports = config;
