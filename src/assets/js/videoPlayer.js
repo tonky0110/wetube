49cef828a983e95994eed7efcba9085112af08bf
Donghwan Seo <tonky0110@gmail.com>
2020-10-19 23:59:15 +0900

    8.3 container.fullScreen() and document.ExitFullScreen()

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
     if (videoPlayer.paused) {
@@ -14,7 +15,6 @@ function handlePlayClick() {
 }
 
 function handleVolumeClick() {
-    console.log('onClick volumn');
     if (videoPlayer.muted) {
         videoPlayer.muted = false;
         volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
@@ -24,9 +24,26 @@ function handleVolumeClick() {
     }
 }
 
+function exitFullScreen() {
+    console.log('exitFullScreen');
+    fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+    fullScrnBtn.addEventListener("click", goFullScreen);
+    document.webkitExitFullscreen();
+}
+
+function goFullScreen() {
+    videoContainer.requestFullscreen();
+    fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+    fullScrnBtn.removeEventListener("click", goFullScreen);
+    fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
+
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScrnBtn.addEventListener("click", goFullScreen);
+
 }
 
 if (videoContainer) {
