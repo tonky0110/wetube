e03eebd017cb218a3fddaf4210e2a2ba63fd741b
Donghwan Seo <tonky0110@gmail.com>
2020-10-21 23:32:42 +0900

    8.5.1 volume pug

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -74,16 +74,20 @@ const formatDate = seconds => {
 }
 
 function setTotalTime() {
-    console.log('videoPlayer.duration: ', videoPlayer.duration);
     const totalTimeString = formatDate(videoPlayer.duration);
-    console.log('totalTimeString: ', totalTimeString);
 
     totalTime.innerHTML = totalTimeString;
     setInterval(getCurrentTime, 1000);
+    videoPlayer.currentTime = 300;
 }
 
 function getCurrentTime() {
-    currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+    currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
+}
+
+function handleEnded() {
+    videoPlayer.currentTime = 0;
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
 
 function init() {
@@ -91,6 +95,7 @@ function init() {
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScrnBtn.addEventListener("click", goFullScreen);
     videoPlayer.addEventListener('loadedmetadata', setTotalTime);
+    videoPlayer.addEventListener('ended', handleEnded);
 }
 
 if (videoContainer) {
