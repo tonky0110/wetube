ec7a0116e78ae0eedadc9371e0472bd1deb98731
Donghwan Seo <tonky0110@gmail.com>
2020-10-21 23:50:37 +0900

    8.6 volume Drag and change volume icon

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -5,7 +5,7 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
-
+const volumeRange = document.getElementById("jsVolume");
 
 
 function handlePlayClick() {
@@ -22,7 +22,9 @@ function handleVolumeClick() {
     if (videoPlayer.muted) {
         videoPlayer.muted = false;
         volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+        volumeRange.value = videoPlayer.volume;
     } else {
+        volumeRange.value = 0;
         videoPlayer.muted = true;
         volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
     }
@@ -90,12 +92,30 @@ function handleEnded() {
     playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
 
+function handleDrag(event) {
+    const {
+        target: {
+            value = 0.5
+        }
+    } = event;
+    videoPlayer.volume = value;
+    if (value >= 0.6) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else if (value >= 0.3) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+    } else {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+    }
+}
+
 function init() {
+    videoPlayer.volume = 0.5;
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScrnBtn.addEventListener("click", goFullScreen);
     videoPlayer.addEventListener('loadedmetadata', setTotalTime);
     videoPlayer.addEventListener('ended', handleEnded);
+    volumeRange.addEventListener('input', handleDrag);
 }
 
 if (videoContainer) {
