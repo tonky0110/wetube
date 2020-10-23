a81966c03796bdd3fcaf0317ea9a9efaa84cfcb4
Donghwan Seo <tonky0110@gmail.com>
2020-10-24 00:48:58 +0900

    9.2 stop recording function, save file, handleVideoData

--- src/assets/js/videoRecorder.js
+++ src/assets/js/videoRecorder.js
@@ -3,15 +3,37 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById('jsVideoPreview');
 
 let streamObject;
+let videoRecorder;
+const handleVideoData = (event) => {
+    console.log(event);
+    const {
+        data: videoFile
+    } = event;
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(videoFile);
+    link.download = 'recorded.webm';
+    document.body.appendChild(link);
+    link.click();
+}
+
+const stopRecording = () => {
+    console.log('stopRecording button click');
+    videoPreview.pause();
+    videoRecorder.stop();
+
+    // reset record button txt, function
+    recordBtn.removeEventListener('click', stopRecording);
+    recordBtn.innerHTML = 'Start Recording';
+    recordBtn.addEventListener('click', getVideo);
+}
 
 const startRecording = () => {
-    console.log('startRecording', streamObject);
-    const videoRecorder = new MediaRecorder(streamObject);
-    console.log('videoRecorder', videoRecorder);
-
-    videoRecorder.start();
-    // videoRecorder.addEventListener('')
+    videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start(); // 주기적으로 실행.
+    videoRecorder.addEventListener('dataavailable', handleVideoData);
+    recordBtn.addEventListener('click', stopRecording);
 }
+
 const getVideo = async () => {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
