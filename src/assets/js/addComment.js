e911951a77150f0660cf60138f0a13a1b0c17c80
Donghwan Seo <tonky0110@gmail.com>
2020-10-25 00:21:51 +0900

    10.4 add comment count comment number(unsync)

--- src/assets/js/addComment.js
+++ src/assets/js/addComment.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
 
 const addCommentForm = document.getElementById('jsAddComment');
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById('jsCommentNumber');
 
+const increaseNumber = () => {
+    commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+}
+const addComment = comment => {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerHTML = comment;
+    li.appendChild(span);
+    console.log('li: ', li);
+    commentList.prepend(li);
+    console.log('commentList: ', commentList);
+    increaseNumber();
+}
 const sendComment = async (comment) => {
     const videoId = window.location.href.split('/videos/')[1];
     const response = await axios({
@@ -11,7 +26,10 @@ const sendComment = async (comment) => {
             comment
         }
     });
-    console.log('response: ', response);
+
+    if (response.status === 200) {
+        addComment(comment);
+    }
 }
 const handleSubmit = (event) => {
     event.preventDefault();
